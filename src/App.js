91093651d253353cea9435c3e0c7e9afa7e982ab
Christopher Olano <olanochristopher@gmail.com>
2023-03-28 00:38:15 +0800

    upgrade react from 17 to 18

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
-import "./App.css";
-import AboutPage from "./pages/about/about.component";
-import PortfolioPage from "./pages/portfolio/portfolio.component";
-import ToolsPage from "./pages/tools/tools.component";
-import CertificatePage from "./pages/certificate/certificate.component";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import AboutPage from './pages/about/about.component';
+import PortfolioPage from './pages/portfolio/portfolio.component';
+import ToolsPage from './pages/tools/tools.component';
+import CertificatePage from './pages/certificate/certificate.component';
 
-import Header from "./components/header/header.component";
-import Heading from "./components/heading/heading.component";
-import Section from "./components/section/section.component";
+import Header from './components/header/header.component';
+import Heading from './components/heading/heading.component';
+import Section from './components/section/section.component';
 
 const App = () => {
-    return (
-        <div>
-            <Header />
-            <Heading />
-            <Section />
-            <Switch>
-                <Route exact path="/" component={AboutPage} />
-                <Route exact path="/about" component={AboutPage} />
-                <Route exact path="/portfolio" component={PortfolioPage} />
-                <Route exact path="/tools" component={ToolsPage} />
-                <Route exact path="/certificate" component={CertificatePage} />
-            </Switch>
-        </div>
-    );
+  return (
+    <>
+      <Header />
+      <Heading />
+      <Section />
+      <Routes>
+        <Route exact path='/' element={<AboutPage />} />
+        <Route exact path='/about' element={<AboutPage />} />
+        <Route exact path='/portfolio' element={<PortfolioPage />} />
+        <Route exact path='/tools' element={<ToolsPage />} />
+        <Route exact path='/certificate' element={<CertificatePage />} />
+      </Routes>
+    </>
+  );
 };
 export default App;
--- src/components/card/card-item.component.jsx
+++ src/components/card/card-item.component.jsx
@@ -1,107 +1,80 @@
-import React, { useState } from "react";
-import ReactTooltip from "react-tooltip";
-import Lightbox from "react-image-lightbox";
+import React, { useState } from 'react';
+import { Tooltip as ReactTooltip } from 'react-tooltip';
+import Lightbox from 'react-18-image-lightbox';
 
-import "react-image-lightbox/style.css";
+import 'react-18-image-lightbox/style.css';
 
-import "./card-item.styles.scss";
+import './card-item.styles.scss';
 
 const CardItem = ({ portfolio }) => {
-    const [isOpen, setIsOpen] = useState(false);
-    const [index, setIndex] = useState(0);
-    const {
-        imgUrl,
-        name,
-        shortDescription,
-        gallery,
-        demoVideo,
-        urlProject,
-        repo,
-    } = portfolio;
+  const [isOpen, setIsOpen] = useState(false);
+  const [index, setIndex] = useState(0);
+  const { imgUrl, name, shortDescription, gallery, demoVideo, urlProject, repo } = portfolio;
 
-    const curImage = gallery[index];
-    const nextImage = gallery[(index + 1) % gallery.length];
-    const prevImage = gallery[(index + gallery.length - 1) % gallery.length];
-    return (
-        <div className="card">
-            <div className="front">
-                <img src={imgUrl} alt={name} />
-            </div>
+  const curImage = gallery[index];
+  const nextImage = gallery[(index + 1) % gallery.length];
+  const prevImage = gallery[(index + gallery.length - 1) % gallery.length];
+  return (
+    <div className='card'>
+      <div className='front'>
+        <img src={imgUrl} alt={name} />
+      </div>
 
-            <div className="back">
-                <div className="back-content">
-                    <h2>{name}</h2>
-                    <span>
-                        {name} {shortDescription}
-                    </span>
-                    <div className="sm">
-                        {demoVideo.isAvailable ? (
-                            <a
-                                className="demovid"
-                                data-tip="watch demo via clipchamp"
-                                href={demoVideo.clipchamp}
-                                target="_blank"
-                                rel="noreferrer"
-                            >
-                                <img src="./img/demovid.png" alt="" />
-                            </a>
-                        ) : (
-                            ""
-                        )}
-
-                        <button
-                            type="button"
-                            onClick={() => setIsOpen(true)}
-                            data-tip="show gallery"
-                            href=""
-                        >
-                            <img src="./img/image-gallery.png" alt="" />
-                        </button>
-                        <a
-                            href={urlProject}
-                            target="_blank"
-                            data-tip="navigate to project"
-                            rel="noreferrer"
-                        >
-                            <img src="./img/link.png" alt="" />
-                        </a>
-
-                        {repo.isAvailable ? (
-                            <a
-                                data-tip="Repository Link"
-                                href={repo.github}
-                                target="_blank"
-                                rel="noreferrer"
-                            >
-                                <img src="./img/github.png" alt="" />
-                            </a>
-                        ) : (
-                            ""
-                        )}
-                    </div>
-                </div>
-            </div>
-
-            <ReactTooltip />
-            {isOpen && (
-                <Lightbox
-                    enableZoom={false}
-                    imageTitle={curImage.description}
-                    imagePadding={15}
-                    mainSrc={curImage.path}
-                    nextSrc={nextImage.path}
-                    prevSrc={prevImage.path}
-                    onCloseRequest={() => setIsOpen(false)}
-                    onMovePrevRequest={() =>
-                        setIndex((index + gallery.length - 1) % gallery.length)
-                    }
-                    onMoveNextRequest={() =>
-                        setIndex((index + 1) % gallery.length)
-                    }
-                />
+      <div className='back'>
+        <div className='back-content'>
+          <h2>{name}</h2>
+          <span>
+            {name} {shortDescription}
+          </span>
+          <div className='sm'>
+            {demoVideo.isAvailable ? (
+              <a
+                className='demovid'
+                data-tip='watch demo via clipchamp'
+                href={demoVideo.clipchamp}
+                target='_blank'
+                rel='noreferrer'
+              >
+                <img src='./img/demovid.png' alt='' />
+              </a>
+            ) : (
+              ''
             )}
+
+            <button type='button' onClick={() => setIsOpen(true)} data-tip='show gallery' href=''>
+              <img src='./img/image-gallery.png' alt='' />
+            </button>
+            <a href={urlProject} target='_blank' data-tip='navigate to project' rel='noreferrer'>
+              <img src='./img/link.png' alt='' />
+            </a>
+
+            {repo.isAvailable ? (
+              <a data-tip='Repository Link' href={repo.github} target='_blank' rel='noreferrer'>
+                <img src='./img/github.png' alt='' />
+              </a>
+            ) : (
+              ''
+            )}
+          </div>
         </div>
-    );
+      </div>
+
+      <ReactTooltip />
+      {isOpen && (
+        <Lightbox
+          enableZoom={false}
+          imageTitle={curImage.description}
+          imagePadding={15}
+          mainSrc={curImage.path}
+          nextSrc={nextImage.path}
+          prevSrc={prevImage.path}
+          onCloseRequest={() => setIsOpen(false)}
+          onMovePrevRequest={() => setIndex((index + gallery.length - 1) % gallery.length)}
+          onMoveNextRequest={() => setIndex((index + 1) % gallery.length)}
+        />
+      )}
+    </div>
+  );
 };
 
 export default CardItem;
