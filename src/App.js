8c3736cf1bf861a5bfc60e7147ebe32fa1e53926
Christopher Olano <olanochristopher@gmail.com>
2023-03-31 03:28:36 +0800

    Refactor heading and routes component and use semantic

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
 import './App.scss';
-import AboutPage from './pages/about/about.component';
-import PortfolioPage from './pages/portfolio/portfolio.component';
-import ToolsPage from './pages/tools/tools.component';
-import CertificatePage from './pages/certificate/certificate.component';
 
 import Header from './components/header/header.component';
-import Heading from './components/heading/heading.component';
-import Section from './components/section/section.component';
+import Navigation from './components/navigation/navigation.component';
+import Main from './components/main/main.component';
 
 const App = () => {
   return (
     <>
       <Header />
-      <Heading />
-      <Section />
-      <Routes>
-        <Route exact path='/' element={<AboutPage />} />
-        <Route exact path='/about' element={<AboutPage />} />
-        <Route exact path='/portfolio' element={<PortfolioPage />} />
-        <Route exact path='/tools' element={<ToolsPage />} />
-        <Route exact path='/certificate' element={<CertificatePage />} />
-      </Routes>
+      <Navigation />
+      <Main />
     </>
   );
 };
