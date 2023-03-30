78941dcc9dbd373ff0b76c86922409e02c32b75f
Christopher Olano <olanochristopher@gmail.com>
2023-03-30 23:33:36 +0800

    transform toolspage from class to function component Refactor : use tool component

--- src/pages/tools/tools.component.jsx
+++ src/pages/tools/tools.component.jsx
@@ -1,64 +1,47 @@
 import React from 'react';
 import './tools.styles.scss';
-import { ReactSVG } from 'react-svg';
+import Tool from '../../components/tool/tool.component';
 
-class ToolsPage extends React.Component {
-  constructor() {
-    super();
+const ToolsPage = () => {
+  const tools = [
+    'JavaScript',
+    'ReactJs',
+    'Redux',
+    'SASS',
+    'Jest',
+    'jQuery',
+    'NodeJs',
+    'ExpressJs',
+    'Git',
+    'PHP',
+    'Laravel',
+    'codeIgniter',
+    'MySql',
+    'phpMyAdmin',
+    'BootStrap',
+    'Gitlab',
+    'Github',
+    'Heroku',
+    'HTML',
+    'CSS',
+    'Firebase',
+    'AWS',
+    'MongoDB',
+    'GoDaddy',
+    'Cpanel',
+    'Postman',
+    'Netlify',
+    'LiquidWeb',
+    'VsCode',
+  ];
 
-    this.state = {
-      tools: [
-        'JavaScript',
-        'ReactJs',
-        'Redux',
-        'SASS',
-        'Jest',
-        'jQuery',
-        'NodeJs',
-        'ExpressJs',
-        'Git',
-        'PHP',
-        'Laravel',
-        'codeIgniter',
-        'MySql',
-        'phpMyAdmin',
-        'BootStrap',
-        'Gitlab',
-        'Github',
-        'Heroku',
-        'HTML',
-        'CSS',
-        'Firebase',
-        'AWS',
-        'MongoDB',
-        'GoDaddy',
-        'Cpanel',
-        'Postman',
-        'Netlify',
-        'LiquidWeb',
-        'VsCode',
-      ],
-    };
-  }
-  render() {
-    const tools = this.state.tools;
-
-    return (
-      <div className='tools fade-in'>
-        {tools.map((tool) => (
-          <div className='tool'>
-            <div className='tool-svg__box'>
-              <ReactSVG
-                className='tool-svg__svg'
-                src={`img/tools/svgs/${tool.toLocaleLowerCase()}.svg`}
-              />
-            </div>
-            <span className='tool-name'>{tool}</span>
-          </div>
-        ))}
-      </div>
-    );
-  }
-}
+  return (
+    <section className='tools fade-in'>
+      {tools.map((tool) => (
+        <Tool tool={tool} />
+      ))}
+    </section>
+  );
+};
 
 export default ToolsPage;
