54877b742bce489d883de48851ffa9a98c76c1b9
Christopher Olano <olanochristopher@gmail.com>
2023-03-30 23:32:39 +0800

    create tool component

---
+++ src/components/tool/tool.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ReactSVG } from 'react-svg';
+import './tool.styles.scss';
+
+const Tool = ({ tool }) => {
+  return (
+    <div className='tool'>
+      <div className='tool-svg__box'>
+        <ReactSVG
+          className='tool-svg__svg'
+          src={`img/tools/svgs/${tool.toLocaleLowerCase()}.svg`}
+        />
+      </div>
+      <span className='tool-name'>{tool}</span>
+    </div>
+  );
+};
+
+export default Tool;
