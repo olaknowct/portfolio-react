2466dc54081f1a3476f33ae19bd2ac9ff0b00557
Christopher Olano <olanochristopher@gmail.com>
2023-04-24 20:09:40 +0800

    add tools and remove links unneccesary

--- src/components/header/nav-header/nav-header.component.jsx
+++ src/components/header/nav-header/nav-header.component.jsx
@@ -3,16 +3,16 @@ import './nav-header.styles.scss';
 const NavHeader = () => {
   return (
     <nav className='header-nav'>
-      <a href='https://www.facebook.com/christopher.olano.1/' target='_blank' rel='noreferrer'>
+      <a href='#facebook'>
         <img className='nav-img fb' src='./img/facebook.png' alt='' />
       </a>
       <a href='https://github.com/olaknowct' target='_blank' rel='noreferrer'>
         <img className='nav-img github' src='./img/github.png' alt='' />
       </a>
-      <a href='https://twitter.com/ola_knowsIt' target='_blank' rel='noreferrer'>
+      <a href='#twitter'>
         <img className='nav-img twtr' src='./img/twitter.png' alt='' />
       </a>
-      <a href='https://www.instagram.com/ola_knowsit/?hl=en' target='_blank' rel='noreferrer'>
+      <a href='#ig'>
         <img className='nav-img ig' src='./img/instagram.png' alt='' />
       </a>
       <a
